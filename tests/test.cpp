b9d79ac71a1c1bc420476dd383635bb85d86c36c
hongshibao <shibaohong@outlook.com>
2022-10-29 19:10:49 +0800

    Add unit tests using catch2

---
+++ tests/test.cpp
@@ -0,0 +1,121 @@
+#include <singleflight/singleflight.h>
+#include <spdlog/spdlog.h>
+
+#include <atomic>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+#include <catch2/matchers/catch_matchers_exception.hpp>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+using namespace std;
+using namespace singleflight;
+using Catch::Matchers::Message;
+
+TEST_CASE("Multiple threads call a same func using a same key") {
+    SingleFlight<string, string> sf;
+    atomic_int func_call_cnt{0};
+
+    // Simulate a heavy function call
+    const string LONG_RUNNING_FUNC_RESULT = "Result from long_running_func";
+    auto long_running_func = [&](int tid) -> string {
+        ++func_call_cnt;
+        spdlog::info("long_running_func call by Thread {}", tid);
+        this_thread::sleep_for(1000ms);
+        return LONG_RUNNING_FUNC_RESULT;
+    };
+
+    // Thread entry function
+    auto thread_entry_func = [&](int tid) {
+        spdlog::info("Thread {} starts", tid);
+        auto res = sf.Do("some-key", long_running_func, tid);
+        spdlog::info("Thread {} result: {}", tid, res);
+        REQUIRE(res == LONG_RUNNING_FUNC_RESULT);
+    };
+
+    // Launch threads
+    constexpr int THREADS_NUM = 5;
+    vector<shared_ptr<thread>> threads;
+    for (int i = 0; i < THREADS_NUM; ++i) {
+        threads.push_back(make_shared<thread>(thread_entry_func, i));
+    }
+    // Waiting
+    for (auto t : threads) {
+        t->join();
+    }
+
+    // long_running_func should only be called once
+    REQUIRE(func_call_cnt.load() == 1);
+}
+
+TEST_CASE("Multiple threads call a same func which throws std::exception") {
+    SingleFlight<string, string> sf;
+    atomic_int func_call_cnt{0};
+
+    // Simulate a function call which throws std::exception
+    auto throwing_exception_func = [&](int tid) -> string {
+        ++func_call_cnt;
+        spdlog::info("throwing_exception_func call by Thread {}", tid);
+        this_thread::sleep_for(500ms);
+        throw runtime_error{"std::runtime_error from throwing_exception_func"};
+        return "Result from throwing_exception_func";
+    };
+
+    // Thread entry function
+    auto thread_entry_func = [&](int tid) {
+        spdlog::info("Thread {} starts", tid);
+        REQUIRE_THROWS_AS(sf.Do("some-key", throwing_exception_func, tid), FuncCallFailedException);
+    };
+
+    // Launch threads
+    constexpr int THREADS_NUM = 5;
+    vector<shared_ptr<thread>> threads;
+    for (int i = 0; i < THREADS_NUM; ++i) {
+        threads.push_back(make_shared<thread>(thread_entry_func, i));
+    }
+    // Waiting
+    for (auto t : threads) {
+        t->join();
+    }
+
+    // long_running_func should only be called once
+    REQUIRE(func_call_cnt.load() == 1);
+}
+
+TEST_CASE("Multiple threads call a same func which throws non std::exception") {
+    SingleFlight<string, string> sf;
+    atomic_int func_call_cnt{0};
+
+    // Simulate a function call which throws std::exception
+    auto throwing_exception_func = [&](int tid) -> string {
+        ++func_call_cnt;
+        spdlog::info("throwing_exception_func call by Thread {}", tid);
+        this_thread::sleep_for(500ms);
+        throw "non std::exception from throwing_exception_func";
+        return "Result from throwing_exception_func";
+    };
+
+    // Thread entry function
+    auto thread_entry_func = [&](int tid) {
+        spdlog::info("Thread {} starts", tid);
+        REQUIRE_THROWS_MATCHES(sf.Do("some-key", throwing_exception_func, tid),
+                               FuncCallFailedException,
+                               Message("Func call threw non-std-exception"));
+    };
+
+    // Launch threads
+    constexpr int THREADS_NUM = 5;
+    vector<shared_ptr<thread>> threads;
+    for (int i = 0; i < THREADS_NUM; ++i) {
+        threads.push_back(make_shared<thread>(thread_entry_func, i));
+    }
+    // Waiting
+    for (auto t : threads) {
+        t->join();
+    }
+
+    // long_running_func should only be called once
+    REQUIRE(func_call_cnt.load() == 1);
+}
